25519ba2d7dd9dddb29a86c13082b9f6f2ce49af
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-26 22:38:40 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,17 +15,26 @@ class Artist
 
   def save
     @@all << self
+    #saves new instances of artists to the class variable @@all = []
   end
 
   def self.all
     @@all
+    #returns the collection of artists from the class variable, @@all = []
   end
 
-  def find_or_create_by_name
+  def self.find_or_create_by_name(name)
+    self.all.find { |artist| artist.name == name} || Artist.new(name)
+    #self.all-> take saved artists from the @@all = []
+    #find / detect-> iterate through the @@all = []
+    #artist.name == name-> when a match is found, add it & return it
+    #If a match isn't found, the left-side of the l|OR|r will evaluate to false,
+    #the process will continue to the right side thus creating a new artist instance via Artist.new(name)
+  end
 
+  def print_songs
     @songs.each { |song| puts song.name }
+    #iterates through the instance @songs array  & puts all the songs
   end
-
-
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,13 @@ class Song
   attr_accessor :name, :artist
 
   def initialize(name)
-    @name = name  #song name
+    @name = name  #automatically require a song name for every new instance that's instantiated
   end
 
   def self.new_by_filename(file_name)
     song_name = file
   end
+
+
 
 end
