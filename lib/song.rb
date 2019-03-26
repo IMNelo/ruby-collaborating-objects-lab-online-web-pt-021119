0168d9acc4aebd69feff57ff88e00b92708c9e50
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-26 23:22:46 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,13 @@ class Song
     @name = name  #automatically require a song name for every new instance that's instantiated
   end
 
-  def self.new_by_filename(file_name)
-    song_name = file
+  def self.new_by_filename(filename)  #class method
+    split_name = filename.split(" - ")
+    song = self.new(split_name[1])
+    artist = Artist.find_or_create_by_name(split_name[0])
+    song.artist = artist
+    artist.add_song(song)
+    song
   end
-
-
 
 end
