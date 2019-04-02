112f5f5d48bfd2bcc6f89713d01a2203078bb70b
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-04-02 07:37:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,12 +7,11 @@ class Song
   end
 
   def self.new_by_filename(filename)  #class method
-    split_name = filename.split(" - ")
-    song = self.new(split_name[1])
-    artist = Artist.find_or_create_by_name(split_name[0])
-    song.artist = artist
-    artist.add_song(song)
-    song
+    artist, song = filename.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song.save
+
   end
 
 end
